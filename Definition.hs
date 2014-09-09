dc38f8888d0c567ed0920f3a58365d96bf479365
Lars Fischer <fischer@wiwi.uni-siegen.de>
2014-09-09 23:32:53 +0200

    filtering empty lines after parsing. Don't know how to integrate this in parsec parser

--- Definition.hs
+++ Definition.hs
@@ -2,6 +2,7 @@ module Definition (parseDescr, parseDescrFromFile, DescrLine, image, alttxt, txt
 
 import Data.List
 import Text.ParserCombinators.Parsec
+import Debug.Trace
 
 import FileName
 
@@ -17,26 +18,30 @@ data DescrLine = DescrLine {
   alttxt  :: String } deriving Show
 
 packDescr:: [String] -> DescrLine
-packDescr (i:p:n:a:t:_) = DescrLine (read i) (read p) (read n) (read t) a
+packDescr (i:p:n:a:t:_) = trace (show $ DescrLine (read i) (read p) (read n) (read t) a) DescrLine (read i) (read p) (read n) (read t) a
+packDescr x = error $ "packDescr: wrong number of fields in" ++ (show x)
 -- packDescr i:p:n:t:as = DescrLine i p n t $ concat as
 
 
 {- Description File Parser using Parsec
 source: http://book.realworldhaskell.org/read/using-parsec.html
+TODO: make parser accept and ignore empty lines
+TODO: accept missing \n on last line
 -}
 
-descrFile = endBy line eol
-line = sepBy cell (char ';')
+descrFile = sepEndBy line eol
+line = sepBy cell (char ';') 
+--emptyline = spaces
 cell = many (noneOf ";\n")
-eol = char '\n'
+eol = char '\n' 
 
 parseDescr :: String -> Either ParseError [DescrLine]
 parseDescr input = case parse descrFile "(unknown)" input of
   (Left e)  -> Left e
-  (Right l) -> Right $ map packDescr l
+  (Right l) -> Right $ map packDescr $ filter (/= [""]) l
 
 
 parseDescrFromFile:: String -> IO (Either ParseError [DescrLine])
 parseDescrFromFile infile = parseFromFile descrFile infile
                             >>= (\e -> return $
-                                       e >>= return.(map packDescr)) 
+                                       e >>= return.(map packDescr).(filter (/= [""])) ) 
